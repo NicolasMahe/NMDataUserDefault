d78c3291e15266469ebe0309cdcc473ff385c0c3
Nicolas Mahé <nicolas@mahe.me>
2017-03-10 10:40:13 +0700

    Release 0.0.3.

--- NMDataUserDefault/NMDataUserDefaultArrayEnum.swift
+++ NMDataUserDefault/NMDataUserDefaultArrayEnum.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 
-public class NMDataUserDefaultArrayEnum<T: RawRepresentable>: NSObject, NMDataUserDefaultProtocol {
+public class NMDataUserDefaultArrayEnum<T: RawRepresentable>: NSObject {//, NMDataUserDefaultProtocol {
   
   public typealias ValueType = [T]?
   
@@ -62,5 +62,52 @@ public class NMDataUserDefaultArrayEnum<T: RawRepresentable>: NSObject, NMDataUs
       }
     }
     return nil
+  }//----------------------------------------------------------------------------
+  // MARK: - Notification
+  //----------------------------------------------------------------------------
+  
+  public var notification: Notification.Name {
+    return Notification.Name(rawValue: self.identifier)
+  }
+  
+  //----------------------------------------------------------------------------
+  // MARK: - Value
+  //----------------------------------------------------------------------------
+  
+  public var value: ValueType {
+    get {
+      //get from memory
+      if self.enableInMemory == true,
+        let value = self._value {
+        return value
+      }
+      
+      if let value = self.unarchive() {
+        self._value = value
+        return value
+      }
+      
+      //get default asset
+      return self.defaultValue
+    }
+    set(value) {
+      //Set in memory
+      self._value = value
+      
+      if let data = self.archive(value) {
+        self.store.set(data, forKey: self.identifier)
+      }
+      else {
+        self.store.removeObject(forKey: self.identifier)
+      }
+      
+      //send notif
+      NotificationCenter.default.post(
+        name: self.notification,
+        object: nil
+      )
+      
+      self.onChange?()
+    }
   }
 }
